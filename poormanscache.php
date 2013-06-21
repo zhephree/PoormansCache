80ee2db0b9108b32ea42d0ca8e7e64333e20f38c
Geoff Gauchet <geoff@zhephree.com>
2013-06-20 23:49:19 -0500

    Create poormanscache.php

---
+++ poormanscache.php
@@ -0,0 +1,71 @@
+<?php
+class PoormansCache {
+  protected $path;
+	
+	function __construct($path='./cache'){
+		$this->path=$path;
+	}
+	
+	function store($key,$value){
+		$filename=md5($key);
+		$what=serialize($value);
+		$fullpath=$this->path.'/'.$filename;
+		$updateFile=false;
+		
+		$fh=fopen($fullpath, 'w');
+		fwrite($fh, $what);
+		fclose($fh);
+	}
+	
+	function get($key){
+		$filename=md5($key);
+		$fullpath=$this->path.'/'.$filename;
+		if(file_exists($fullpath)){
+			$fh=fopen($fullpath, 'r');
+			$contents = fread($fh, filesize($fullpath));
+			fclose($fh);
+			return unserialize($contents);
+		}else{
+			return false;
+		}
+	}
+	
+	function clear($key){
+		$filename=md5($key);
+		$fullpath=$this->path.'/'.$filename;
+		if(file_exists($fullpath)){
+			if(unlink($fullpath)){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+	
+	function age($key){
+		$filename=md5($key);
+		$fullpath=$this->path.'/'.$filename;
+		if(file_exists($fullpath)){
+			$filedate=filemtime($fullpath);
+			$now=time();
+			
+			$diff=$now-$filedate;
+			$mins=ceil($diff/60);
+			return $mins;
+		}else{
+			return -1;
+		}
+	}
+	
+	function is_old($key,$maxAge){
+		$age=$this->age($key);
+		if($age>=$maxAge || $age==-1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+?>
