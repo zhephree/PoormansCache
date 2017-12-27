8bebd2a594d356989902b8cda0903c9dbd9270c3
Geoff Gauchet <geoff@zhephree.com>
2017-12-27 12:26:08 -0600

    Update poormanscache.php
    formatting

--- poormanscache.php
+++ poormanscache.php
@@ -1,80 +1,90 @@
 <?php
 /**
  *  Poorman's Cache v1 - Decent PHP caching
- * 
+ *
  *  @author Geoffrey Gauchet <geoff@zhephree.com>
  *  @link http://zhephree.com Website
  *  @link http://github.com/zhephree GitHub Repositories
  *  @license GPL, v2
  */
+class PoormansCache
 
-class PoormansCache {
-  protected $path;
-	
-	function __construct($path='./cache'){
-		$this->path=$path;
+{
+	protected $path;
+	function __construct($path = './cache')
+	{
+		$this->path = $path;
 	}
-	
-	function store($key,$value){
-		$filename=md5($key);
-		$what=serialize($value);
-		$fullpath=$this->path.'/'.$filename;
-		$updateFile=false;
-		
-		$fh=fopen($fullpath, 'w');
+
+	function store($key, $value)
+	{
+		$filename = md5($key);
+		$what = serialize($value);
+		$fullpath = $this->path . '/' . $filename;
+		$updateFile = false;
+		$fh = fopen($fullpath, 'w');
 		fwrite($fh, $what);
 		fclose($fh);
 	}
-	
-	function get($key){
-		$filename=md5($key);
-		$fullpath=$this->path.'/'.$filename;
-		if(file_exists($fullpath)){
-			$fh=fopen($fullpath, 'r');
+
+	function get($key)
+	{
+		$filename = md5($key);
+		$fullpath = $this->path . '/' . $filename;
+		if (file_exists($fullpath)) {
+			$fh = fopen($fullpath, 'r');
 			$contents = fread($fh, filesize($fullpath));
 			fclose($fh);
 			return unserialize($contents);
-		}else{
+		}
+		else {
 			return false;
 		}
 	}
-	
-	function clear($key){
-		$filename=md5($key);
-		$fullpath=$this->path.'/'.$filename;
-		if(file_exists($fullpath)){
-			if(unlink($fullpath)){
+
+	function clear($key)
+	{
+		$filename = md5($key);
+		$fullpath = $this->path . '/' . $filename;
+		if (file_exists($fullpath)) {
+			if (unlink($fullpath)) {
 				return true;
-			}else{
+			}
+			else {
 				return false;
 			}
-		}else{
+		}
+		else {
 			return false;
 		}
 	}
-	
-	function age($key){
-		$filename=md5($key);
-		$fullpath=$this->path.'/'.$filename;
-		if(file_exists($fullpath)){
-			$filedate=filemtime($fullpath);
-			$now=time();
-			
-			$diff=$now-$filedate;
-			$mins=ceil($diff/60);
+
+	function age($key)
+	{
+		$filename = md5($key);
+		$fullpath = $this->path . '/' . $filename;
+		if (file_exists($fullpath)) {
+			$filedate = filemtime($fullpath);
+			$now = time();
+			$diff = $now - $filedate;
+			$mins = ceil($diff / 60);
 			return $mins;
-		}else{
+		}
+		else {
 			return -1;
 		}
 	}
-	
-	function is_old($key,$maxAge){
-		$age=$this->age($key);
-		if($age>=$maxAge || $age==-1){
+
+	function is_old($key, $maxAge)
+	{
+		$age = $this->age($key);
+		if ($age >= $maxAge || $age == - 1) {
 			return true;
-		}else{
+		}
+		else {
 			return false;
 		}
 	}
 }
+
 ?>
