87169c06cf3dc17bf2ff9998104bfd1a2a2c340a
Geoffrey Gauchet <geoff@zhephree.com>
2026-04-14 11:14:10 -0500

    Merge in updates from outside projects: - feat: allow keys to be unhashed - feat: wildcards can be used for clearing caches

--- poormanscache.php
+++ poormanscache.php
@@ -7,81 +7,100 @@
  *  @link http://github.com/zhephree GitHub Repositories
  *  @license GPL, v2
  */
-class PoormansCache
-
-{
+class PoormansCache {
 	protected $path;
-	function __construct($path = './cache')
-	{
+	protected $hashKeys = false;
+	
+	function __construct($path = './cache', $hashKeys = false){
 		$this->path = $path;
+		$this->hashKeys = $hashKeys;
 	}
-
-	function store($key, $value)
-	{
-		$filename = md5($key);
+	
+	function store($key, $value){
+		$filename = $this->hashKeys? md5($key): basename($key);
 		$what = serialize($value);
 		$fullpath = $this->path . '/' . $filename;
-		$updateFile = false;
+		
 		$fh = fopen($fullpath, 'w');
 		fwrite($fh, $what);
 		fclose($fh);
+		unset($fh);
 	}
-
-	function get($key)
-	{
-		$filename = md5($key);
+	
+	function get($key){
+		$filename = $this->hashKeys? md5($key) : basename($key);
 		$fullpath = $this->path . '/' . $filename;
-		if (file_exists($fullpath)) {
+		if(file_exists($fullpath)){
 			$fh = fopen($fullpath, 'r');
-			$contents = fread($fh, filesize($fullpath));
-			fclose($fh);
-			return unserialize($contents);
-		}
-		else {
-			return false;
-		}
-	}
-
-	function clear($key)
-	{
-		$filename = md5($key);
-		$fullpath = $this->path . '/' . $filename;
-		if (file_exists($fullpath)) {
-			if (unlink($fullpath)) {
-				return true;
-			}
-			else {
+			$filesize = filesize($fullpath);
+			if($filesize > 0){
+				$contents = fread($fh, $filesize);
+				fclose($fh);
+				unset($fh);
+				return unserialize($contents);
+			}else{
+				fclose($fh);
+				unset($fh);
 				return false;
 			}
-		}
-		else {
+		}else{
 			return false;
 		}
 	}
+	
+	function clear($key){
+		if((strpos($key, '*') !== false || strpos($key, '?') !== false) && $this->hashKeys){
+			throw new Exception('Wildcard (*,?) use in keys is not supported when hashKeys is true.');
+		}
 
-	function age($key)
-	{
-		$filename = md5($key);
+		if((strpos($key, '*') !== false || strpos($key, '?') !== false) && !$this->hashKeys){
+			$returnValue = false;
+			foreach(glob($this->path . '/' .$key) as $fullpath){
+				if(file_exists($fullpath)){
+					if(unlink($fullpath)){
+						$returnValue =  true;
+					}
+				}		
+			}
+
+
+			return $returnValue;
+		}else{
+			$filename = $this->hashKeys? md5($key) : basename($key);
+			$fullpath = $this->path . '/' . $filename;
+			if(file_exists($fullpath)){
+				if(unlink($fullpath)){
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return false;
+			}	
+		}
+
+	}
+	
+	function age($key){
+		$filename = $this->hashKeys? md5($key) : basename($key);
 		$fullpath = $this->path . '/' . $filename;
-		if (file_exists($fullpath)) {
+		if(file_exists($fullpath)){
 			$filedate = filemtime($fullpath);
+			
 			$now = time();
 			$diff = $now - $filedate;
 			$mins = ceil($diff / 60);
 			return $mins;
-		}
-		else {
+		}else{
 			return -1;
 		}
 	}
-
-	function is_old($key, $maxAge)
-	{
+	
+	function is_old($key,$maxAge){
 		$age = $this->age($key);
-		if ($age >= $maxAge || $age == - 1) {
+		if($age >= $maxAge || $age == -1){
 			return true;
-		}
-		else {
+		}else{
 			return false;
 		}
 	}
